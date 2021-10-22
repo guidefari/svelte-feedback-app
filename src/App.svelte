822bc0497bc9ca85414716ac7574642549199604
Guide Fari <guideg6@gmail.com>
2021-10-22 11:18:46 +0200

    feat: named function on click + reactive value

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,21 @@
 <script>
-	export let name;
+	let firstName = 'Guide';
+	let lastName = 'Fari';
+	let color = 'maroon'
+
+	// reactive value
+	$: name = firstName + ' ' + lastName
+
+	const toggle = () => {
+		color = color === 'blue' ? 'purple' :  'blue'
+	}
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
+	<h1 style="color: {color}">Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+
+	<button on:click={toggle}>Change color</button>
 </main>
 
 <style>
